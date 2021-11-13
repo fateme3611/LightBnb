984b36c9ba7bda69156fde334eff3c6460e01be2
fateme <fateme3611@yahoo.com>
2021-11-12 23:23:14 -0500

    create  tabels

---
+++ migrations/01_schema.sql
@@ -0,0 +1,45 @@
+DROP TABLE if EXISTS property_reviews;
+DROP TABLE if EXISTS reservations;
+DROP TABLE if EXISTS properties;
+DROP TABLE if EXISTS users;
+
+CREATE TABLE users(
+id SERIAL PRIMARY KEY,
+"name" VARCHAR(255),
+email VARCHAR(255),
+"password" VARCHAR(255)
+);
+CREATE TABLE properties(
+id SERIAL PRIMARY KEY,
+owner_id INT REFERENCES users(id),
+title VARCHAR(255),
+description VARCHAR(255),
+thumbnail_photo_url VARCHAR(255),
+cover_photo_url VARCHAR(255),
+cost_per_night INT,
+number_of_bedrooms INT,
+country VARCHAR(255),
+street VARCHAR(255),
+city VARCHAR(255),
+province VARCHAR(255),
+post_code VARCHAR(255),
+active bool
+);
+
+CREATE TABLE reservations(
+id SERIAL PRIMARY KEY,
+start_date date,
+end_date date,
+property_id INT REFERENCES properties(id),
+guest_id INT REFERENCES users(id)
+);
+
+CREATE TABLE property_reviews(
+id SERIAL PRIMARY KEY,
+guest_id INT REFERENCES users(id),
+property_id INT REFERENCES properties(id),
+reservation_id INT REFERENCES reservations(id),
+rating INT,
+message VARCHAR(255)
+);
+
