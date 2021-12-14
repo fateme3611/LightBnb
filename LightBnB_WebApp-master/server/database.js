0d83f115301afb52f1bd86f38802b290fbf377db
fateme <fateme3611@yahoo.com>
2021-12-13 19:43:03 -0500

    update functions with SQL queries

---
+++ LightBnB_WebApp-master/server/database.js
@@ -0,0 +1,147 @@
+const properties = require('./json/properties.json');
+const users = require('./json/users.json');
+
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '',
+  host: '192.168.20.200',
+  database: 'LighthouseBnb'
+});
+/// Users
+
+/**
+ * Get a single user from the database given their email.
+ * @param {String} email The email of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithEmail = function (email) {
+  return pool
+    .query(`SELECT * FROM Users WHERE email = $1 limit 1`, [email])
+    .then((result) => {
+      if (result.rows.length) {
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+}
+
+exports.getUserWithEmail = getUserWithEmail;
+
+/**
+ * Get a single user from the database given their id.
+ * @param {string} id The id of the user.
+ * @return {Promise<{}>} A promise to the user.
+ */
+const getUserWithId = function (id) {
+  return pool
+    .query(`SELECT top 1 * FROM Users WHERE id = $1`, [id])
+    .then((result) => {
+      if (result.rows.length) {
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+exports.getUserWithId = getUserWithId;
+
+
+/**
+ * Add a new user to the database.
+ * @param {{name: string, password: string, email: string}} user
+ * @return {Promise<{}>} A promise to the user.
+ */
+const addUser = function (user) {
+  return pool
+    .query(`INSERT INTO Users 
+      ("name", "email", "password")
+      values ($1, $2, $3)  RETURNING *;` , [user.name, user.email, user.password])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+exports.addUser = addUser;
+
+/// Reservations
+
+/**
+ * Get all reservations for a single user.
+ * @param {string} guest_id The id of the user.
+ * @return {Promise<[{}]>} A promise to the reservations.
+ */
+const getAllReservations = function (guest_id, limit = 10) {
+  return pool
+    .query(`SELECT * FROM Reservations LIMIT $1`, [limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+}
+exports.getAllReservations = getAllReservations;
+
+/// Properties
+
+/**
+ * Get all properties.
+ * @param {{}} options An object containing query options.
+ * @param {*} limit The number of results to return.
+ * @return {Promise<[{}]>}  A promise to the properties.
+ */
+const getAllProperties = function (options, limit = 10) {
+
+
+  return pool
+    .query(`SELECT * FROM Properties LIMIT $1`, [limit])
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+
+}
+exports.getAllProperties = getAllProperties;
+
+
+/**
+ * Add a property to the database
+ * @param {{}} property An object containing all of the property details.
+ * @return {Promise<{}>} A promise to the property.
+ */
+const addProperty = function (property) {
+  return pool
+    .query(`INSERT INTO Properties 
+     ("owner_id", "title", "description", "thumbnail_photo_url", 
+     cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, 
+     number_of_bedrooms, country , street, city,
+     province, post_code, active )
+      values 
+      ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)  RETURNING *;` ,
+      [property.owner_id, property.title, property.password, property.description,
+      property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms,
+      property.number_of_bedrooms, property.country, property.street, property.city,
+      property.province, property.post_code, property.active
+      ])
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+exports.addProperty = addProperty;
